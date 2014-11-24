8dcad6fc04e9011cf8550d6feafabbdf4fcc8441
Simon Gomez de Kset <simongomezdekset@Simons-MacBook-Air-2.local>
2014-11-24 11:55:20 +0800

    locations branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121041831) do
+ActiveRecord::Schema.define(version: 20141123125457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.integer  "trip_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "locations", ["trip_id"], name: "index_locations_on_trip_id", using: :btree
 
   create_table "trip_users", force: true do |t|
     t.integer  "user_id"
@@ -24,12 +33,12 @@ ActiveRecord::Schema.define(version: 20141121041831) do
   end
 
   create_table "trips", force: true do |t|
-    t.string   "location"
     t.string   "owner"
     t.date     "start_date"
     t.date     "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "users", force: true do |t|
